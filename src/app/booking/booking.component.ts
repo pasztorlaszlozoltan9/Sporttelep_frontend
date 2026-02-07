2e78d22199dfecd90bb57fb9e1a2bf6b860f29a7
Balog <baloglevente.work@gmail.com>
2026-02-07 09:25:12 +0100

    booking

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BookingService } from '../shared/booking.service';
 
 @Component({
   selector: 'app-booking',
   standalone: true,
-  imports: [],
   templateUrl: './booking.component.html',
-  styleUrl: './booking.component.css'
+  styleUrls: ['./booking.component.css']
 })
 export class BookingComponent {
 
+  sportId!: number;
+  locationId!: number;
+  fieldId!: number;
+
+  freeTimes: any[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private bookingService: BookingService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.sportId = params['sportId'];
+      this.locationId = params['locationId'];
+      this.fieldId = params['fieldId'];
+
+      this.loadFreeTimes();
+    });
+  }
+  loadFreeTimes() {
+    this.bookingService.getFreeTimes(this.fieldId).subscribe(times => {
+      this.freeTimes = times;
+    });
+  }
+
+  confirmBooking(time: string) {
+    const bookingData = {
+      sportId: this.sportId,
+      locationId: this.locationId,
+      fieldId: this.fieldId,
+      startTime: time
+    };
+
+    this.bookingService.createBooking(bookingData).subscribe(() => {
+      alert('Foglalás sikeresen elküldve!');
+    });
+  }
 }
