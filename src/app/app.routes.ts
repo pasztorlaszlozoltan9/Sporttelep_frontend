60830016c114334658e9d8390b76519c6023a2ee
pasztorlaszlo9 <pasztor.laszlo93@gmail.com>
2026-04-07 22:32:12 +0200

    felesleges komponensek törlése

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
-import { LocationsComponent } from './locations/locations.component';
 import { LoginComponent } from './login/login.component';
 import { ContactComponent } from './contact/contact.component';
-import { SportsComponent } from './sports/sports.component';
 import { BookingComponent } from './booking/booking.component';
 import { ProfilComponent } from './profil/profil.component';
 import { AdminComponent } from './admin/admin.component';
@@ -18,10 +16,8 @@ export const routes: Routes = [
     { path: 'main', component: MainComponent },
     { path: 'booking', component: BookingComponent},
     { path: 'payment', component: PaymentComponent},
-    { path: 'locations', component: LocationsComponent},
     { path: 'login', component: LoginComponent},
     { path: 'contact', component: ContactComponent},
-    { path: 'sports', component: SportsComponent},
     { path: 'profil', component: ProfilComponent},
     { path: 'admin', component: AdminComponent, canActivate: [AdminGuard]},
     { path: '', component: RegisterComponent },
